e2186ed7ff0ce9b7f0c020a4eab1f271f5169980
mark.texx <marco.tex@live.com>
2022-02-06 18:14:02 +0100

    implemented simple mqtt client in typescript.

--- subscriber/main.ts
+++ subscriber/main.ts
@@ -1,1 +1,23 @@
-console.log('ciao mondo');
+import dotenv from 'dotenv';
+import mqtt from 'mqtt';
+
+dotenv.config();
+
+const brokerInfo = {
+    hostname: `mqtt://${process.env.BROKER_IP}`,
+    port: +process.env.BROKER_PORT,
+    timeout: process.env.PUBLISH_TIMEOUT,
+    topic: process.env.TOPIC,
+};
+
+console.log(brokerInfo);
+
+const client = mqtt.connect(brokerInfo.hostname, { port: brokerInfo.port });
+
+client.on('connect', () => {
+    client.subscribe(brokerInfo.topic);
+});
+
+client.on('message', (topic: string, message: Buffer) => {
+    console.log(message.toString());
+});
