4033dcd176083ecdfe8ae6043797a882c02e90fd
mark.texx <marco.tex@live.com>
2023-01-15 14:35:36 +0100

    fixed parser command

--- backend/src/command_controller.py
+++ backend/src/command_controller.py
@@ -1,26 +1,25 @@
 from flask_restful import Resource, abort, reqparse
 
-resources = [
-    "alarm_ecu_toggle",
-    "alarm_ecu_set",
-    "gate_ecu_toggle",
-    "gate_ecu_set",
-    "gate_stop_toggle",
-    "alarm_antipanic_mode",
-]
-
-parser = reqparse.RequestParser()
-for arg in resources:
-    parser.add_argument(arg)
-
 
 class CommandController(Resource):
+    resources = [
+        "alarm_ecu_toggle",
+        "alarm_ecu_set",
+        "gate_ecu_toggle",
+        "gate_ecu_set",
+        "gate_stop_toggle",
+        "alarm_antipanic_mode",
+    ]
+
     def __init__(self, **kwargs) -> None:
         super().__init__()
         self.automation = kwargs['automation']
+        self.parser = reqparse.RequestParser()
+        for arg in self.__class__.resources:
+            self.parser.add_argument(arg)
 
     def put(self):
-        args = parser.parse_args()
+        args = self.parser.parse_args()
 
         if self.is_command_ecu_toggle(args):
             result = self.automation.toggle_alarm_ecu()
@@ -28,7 +27,7 @@ class CommandController(Resource):
             result = self.automation.set_alarm_ecu(int(args['alarm_ecu_set']))
         elif self.is_command_antipanic_mode(args):
             result = self.automation.antipanic_mode()
-        elif self.is_command_gate_stop_set(args):
+        elif self.is_command_gate_toggle(args):
             result = self.automation.toggle_gate_ecu()
         elif self.is_command_gate_set(args):
             result = self.automation.set_gate_ecu(int(args['gate_ecu_set']))
@@ -48,7 +47,7 @@ class CommandController(Resource):
     def is_command_antipanic_mode(self, args) -> bool:
         return args['alarm_antipanic_mode'] is not None
 
-    def is_command_get_toggle(self, args) -> bool:
+    def is_command_gate_toggle(self, args) -> bool:
         return args['gate_ecu_toggle'] is not None
 
     def is_command_gate_set(self, args) -> bool:
