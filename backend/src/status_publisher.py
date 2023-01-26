bb5ebd95767b19a7fbb492fd0416d3affb975531
marco.tessari <marco.tessari@fv.lan>
2023-01-26 10:44:11 +0100

    using client loop start to manage network thread

--- backend/src/status_publisher.py
+++ backend/src/status_publisher.py
@@ -6,9 +6,6 @@ import src.environment as environment
 
 
 class StatusPublisher(Thread):
-    automation = None
-    status_publisher = None
-    command_subscriber = None
 
     def __init__(self, automation):
         Thread.__init__(self)
@@ -16,22 +13,28 @@ class StatusPublisher(Thread):
         self.__create_status_client_publisher()
 
     def __create_status_client_publisher(self) -> None:
-        self.status_publisher = mqtt.Client(client_id=environment.CLIENT_ID, transport="websockets")
-        self.status_publisher.on_connect = self.__on_connect
-        self.status_publisher.on_disconnect = self.__on_disconnect
-        self.status_publisher.connect(environment.BROKER_IP, int(environment.BROKER_PORT), 60)
+        self.connected_flag = False
+        self.client = mqtt.Client(client_id=environment.CLIENT_ID, transport="websockets")
+        self.client.on_connect = self.__on_connect
+        self.client.on_disconnect = self.__on_disconnect
+        self.client.connect(environment.BROKER_IP, int(environment.BROKER_PORT), 60)
 
     def __on_connect(self, client, userdata, flags, rc) -> None:
         logging.debug("Status publisher connected with result code: " + str(rc))
+        self.connected_flag = True
 
     def __on_disconnect(self, client, userdata, rc) -> None:
         logging.warning("Stataus publisher is going to be discconected")
+        self.connected_flag = False
 
     def run(self):
+        self.client.loop_start()
+        while not self.connected_flag:
+            sleep(1)
+
         while True:
             automation_info_serialized = self.automation.serialize()
-
             logging.debug(automation_info_serialized)
-            self.status_publisher.publish(environment.STATUS_TOPIC, automation_info_serialized)
+            self.client.publish(environment.STATUS_TOPIC, automation_info_serialized)
 
             sleep(environment.PUBLISH_TIMEOUT)
