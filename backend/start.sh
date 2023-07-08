022ec39fab91ad656b51e8c487bcb3e4f29723c4
Texx <marco.tex@live.com>
2023-07-08 12:35:23 +0200

    refactored command controller.

--- backend/start.sh
+++ backend/start.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+[ -f .env ] && . ./.env
+[ -f .env.local ] && . ./.env.local
+
+set -e
+
 echo "Activating python virtual environment ..."
 source ./venv/bin/activate
 
-export GPIOZERO_PIN_FACTORY="mock"
+export GPIOZERO_PIN_FACTORY
 
 echo "Starting backend ..."
 python ./main.py
