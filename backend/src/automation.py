8697c9fbe75a62605822703f6731f66ad3b4ece4
Texx <marco.tex@live.com>
2024-01-25 22:51:04 +0100

    wip: remove sys info class and use custom sys info package

--- backend/src/automation.py
+++ backend/src/automation.py
@@ -1,14 +1,27 @@
+import time
 from abc import ABC, abstractmethod
-from src.system_information import SystemInformation
+from system_info_texx.system_info import SystemInfo
+
+from gpiozero import DigitalInputDevice, DigitalOutputDevice
 
 
-class Automation:
+class Automation(ABC):
+
+    _alarm_pin: DigitalInputDevice
+    _ecu_status_pin: DigitalInputDevice
+    _ecu_toggle_pin: DigitalOutputDevice
+
+    _gate_status_pin: DigitalInputDevice
+    _gate_switch_pin: DigitalOutputDevice
+    _gate_stop_pin: DigitalOutputDevice
+
+    _alarm_observers: []
 
     def serialize(self) -> str:
         output = f'{{ {self.__str_alarm_status()}, '
         output += f'{self.__str_ecu_status()}, '
         output += f'{self.__str_gate_status()}, '
-        output += f'"systemInfo": {self.__system_info().serialize()} }}'
+        output += f'"systemInfo": {self.system_info().serialize()} }}'
 
         return output
 
@@ -21,49 +34,78 @@ class Automation:
     def __str_gate_status(self) -> str:
         return f'"gate": {int(self.is_gate_open())}'
 
-    def __system_info(self) -> SystemInformation:
-        system_info = SystemInformation()
-
-        return system_info
-
     @staticmethod
     def is_alarm_ecu_test_mode(prev_state, new_state) -> bool:
         return new_state == prev_state
 
     @abstractmethod
+    def temperature(self) -> (float, str):
+        pass
+
+    @abstractmethod
+    def system_info(self) -> SystemInfo:
+        pass
+
     def is_alarm_ringing(self) -> bool:
-        pass
+        # check alarm
+        if not self._alarm_pin.value:
+            return True
+        else:
+            return False
 
-    @abstractmethod
     def is_alarm_ecu_active(self) -> bool:
-        pass
-
-    @abstractmethod
-    def is_gate_open(self) -> bool:
-        pass
+        # check ecu state
+        if self._ecu_status_pin.value:
+            return True
+        else:
+            return False
 
     def set_alarm_ecu(self, **kwargs) -> bool:
         if self.is_alarm_ecu_active() != bool(int(kwargs['state'])):
             self.toggle_alarm_ecu()
         return self.is_alarm_ecu_active()
 
+    def toggle_alarm_ecu(self, **kwargs) -> bool:
+        # change state ecu toggle pin
+        self._ecu_toggle_pin.on()
+        time.sleep(0.3)
+        self._ecu_toggle_pin.off()
+        time.sleep(0.5)
+
+        return self.is_alarm_ecu_active()
+
+    def antipanic_mode(self, **kwargs) -> bool:
+        # change state ecu toggle pin
+        self._ecu_toggle_pin.on()
+        time.sleep(4)
+        self._ecu_toggle_pin.off()
+        time.sleep(0.5)
+
+        return self.is_alarm_ringing()
+
+    def is_gate_open(self) -> bool:
+        if self._gate_status_pin.value:
+            return True
+        else:
+            return False
+
     def set_gate_ecu(self, **kwargs) -> bool:
         if self.is_gate_open() != bool(int(kwargs['state'])):
             self.toggle_gate_ecu()
         return self.is_gate_open()
 
-    @abstractmethod
-    def toggle_alarm_ecu(self, **kwargs) -> bool:
-        pass
+    def toggle_gate_ecu(self, **kwargs) -> bool:
+        # cambio stato del cancello
+        self._gate_switch_pin.on()
+        time.sleep(0.5)
+        self._gate_switch_pin.off()
 
-    @abstractmethod
-    def antipanic_mode(self) -> None:
-        pass
+        return self.is_gate_open()
 
-    @abstractmethod
-    def toggle_gate_ecu(self) -> bool:
-        pass
+    def stop_gate(self, **kwargs) -> bool:
+        # cambio pin stop cancello
+        self._gate_stop_pin.on()
+        time.sleep(0.5)
+        self._gate_stop_pin.off()
 
-    @abstractmethod
-    def stop_gate(self) -> bool:
-        pass
+        return self.is_gate_open()
