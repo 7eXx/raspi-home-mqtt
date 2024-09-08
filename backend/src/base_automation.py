0ccc83b87e8ee72dd0dad63f5d4b5c25d003a036
marco.tessari <marco.tessari@fv.lan>
2024-09-08 18:36:22 +0200

    refactoring: now trigger wake up url use mac address as parameter

--- backend/src/base_automation.py
+++ backend/src/base_automation.py
@@ -1,3 +1,5 @@
+import urllib
+
 import requests as req
 
 from abc import ABC
@@ -14,16 +16,23 @@ class BaseAutomation(Automation, ABC):
         super().__init__()
 
     def wake_ryzen(self, **kwargs) -> bool:
-        wake_ryzen_url = self.__build_jenkins_trigger_job_url(env.WAKE_RYZEN_JOB_URL, env.WAKE_RYZEN_TOKEN)
+        wake_ryzen_url = self.__build_trigger_job_url(env.RYZEN_MAC_ADDR)
         response = req.get(wake_ryzen_url,timeout=self.REQ_TIMEOUT)
 
         return 200 <= response.status_code < 300
 
     def wake_luigi(self, **kwargs) -> int:
-        wake_luigi_lenovo_url = self.__build_jenkins_trigger_job_url(env.WAKE_LUIGI_LENOVO_JOB_URL, env.WAKE_LUIGI_LENOVO_TOKEN)
+        wake_luigi_lenovo_url = self.__build_trigger_job_url(env.LUIGI_LENOVO_MAC_ADDR)
         response = req.get(wake_luigi_lenovo_url,timeout=self.REQ_TIMEOUT)
 
         return 200 <= response.status_code < 300
 
-    def __build_jenkins_trigger_job_url(self, url: str, token: str):
-        return f"http://{env.JENKINS_USER}:{env.JENKINS_PASS}@{url}?token={token}"
+    def __build_trigger_job_url(self, mac_addr: str):
+        params = {
+            "token": env.WAKE_LAN_TOKEN,
+            "MAC_ADDRESS": mac_addr
+        }
+        url = f"http://{env.JENKINS_USER}:{env.JENKINS_PASS}@{env.WAKE_LAN_JOB_URL}"
+        url += f"?{urllib.parse.urlencode(params)}"
+
+        return url
