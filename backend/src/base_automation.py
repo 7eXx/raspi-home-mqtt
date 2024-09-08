348b304292bd35df4507508d89f4eb9c06298ac2
marco.tessari <marco.tessari@fv.lan>
2024-09-08 12:16:48 +0200

    refactor: parametrize jenkins job url

--- backend/src/base_automation.py
+++ backend/src/base_automation.py
@@ -14,19 +14,16 @@ class BaseAutomation(Automation, ABC):
         super().__init__()
 
     def wake_ryzen(self, **kwargs) -> bool:
-        wake_ryzen_url = self.__build_wake_ryzen_url()
+        wake_ryzen_url = self.__build_jenkins_trigger_job_url(env.WAKE_RYZEN_JOB_URL, env.WAKE_RYZEN_TOKEN)
         response = req.get(wake_ryzen_url,timeout=self.REQ_TIMEOUT)
 
         return 200 <= response.status_code < 300
 
     def wake_luigi(self, **kwargs) -> int:
-        wake_luigi_lenovo_url = self.__build_luigi_lenovo_url()
+        wake_luigi_lenovo_url = self.__build_jenkins_trigger_job_url(env.WAKE_LUIGI_LENOVO_JOB_URL, env.WAKE_LUIGI_LENOVO_TOKEN)
         response = req.get(wake_luigi_lenovo_url,timeout=self.REQ_TIMEOUT)
 
         return 200 <= response.status_code < 300
 
-    def __build_wake_ryzen_url(self):
-        return f"http://{env.JENKINS_USER}:{env.JENKINS_PASS}@{env.WAKE_RYZEN_JOB_URL}?token={env.WAKE_RYZEN_TOKEN}"
-
-    def __build_luigi_lenovo_url(self):
-        return f"http://{env.JENKINS_USER}:{env.JENKINS_PASS}@{env.WAKE_LUIGI_LENOVO_JOB_URL}?token={env.WAKE_LUIGI_LENOVO_TOKEN}"
+    def __build_jenkins_trigger_job_url(self, url: str, token: str):
+        return f"http://{env.JENKINS_USER}:{env.JENKINS_PASS}@{url}?token={token}"
