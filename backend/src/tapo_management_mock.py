b1794a9f43180cf6f5dc7580a47d28cbac5a9047
Texx <marco.tex@live.com>
2025-01-06 15:43:59 +0100

    fix: remove annotation.

--- backend/src/tapo_management_mock.py
+++ backend/src/tapo_management_mock.py
@@ -1,6 +1,4 @@
 
-from typing_extensions import override
-
 from src.tapo_management import TapoManagement
 
 
@@ -9,10 +7,8 @@ class TapoManagementMock(TapoManagement):
     def __init__(self):
         super().__init__()
 
-    @override
     def set_home_mode(self):
         pass
 
-    @override
     def set_away_mode(self):
         pass
