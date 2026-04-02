aa563664ee69947aec0d9107fee64e956e6c07e2
Tex <marco.tessari@frigoveneta.it>
2026-04-02 15:27:27 +0200

    feat: add log for tabo errors.

--- backend/src/tapo_management_impl.py
+++ backend/src/tapo_management_impl.py
@@ -24,8 +24,10 @@ class TapoManagementImpl(TapoManagement):
 
         try:
             self.__tapo_c200 = Tapo(env.TAPO_C200_IP, env.TAPO_USERNAME, env.TAPO_PASSWORD)
-        except:
-            self.logger.warning("Tapo C200 not reachable")
+        except Exception as e:
+            import traceback
+            self.logger.warning("Tapo C200 not reachable %s", e)
+            self.logger.debug(traceback.format_exc())
             self.__tapo_c200 = None
 
     def __try_initialize_tapo_c500(self):
@@ -34,8 +36,10 @@ class TapoManagementImpl(TapoManagement):
 
         try:
             self.__tapo_c500 = Tapo(env.TAPO_C500_IP, env.TAPO_USERNAME, env.TAPO_PASSWORD)
-        except:
-            self.logger.warning("Tapo C500 not reachable")
+        except Exception as e:
+            import traceback
+            self.logger.warning("Tapo C500 not reachable %s", e)
+            self.logger.debug(traceback.format_exc())
             self.__tapo_c500 = None
 
     def set_home_mode(self):
