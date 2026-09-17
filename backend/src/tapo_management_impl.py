6f10429d5c3281679ec177e213a71c8ba88b1792
Mark Tex <marco.tex@live.com>
2026-09-17 22:34:41 +0200

    wip: try tapo initialization now assign a new request avoid token expired after a while.

--- backend/src/tapo_management_impl.py
+++ backend/src/tapo_management_impl.py
@@ -19,9 +19,6 @@ class TapoManagementImpl(TapoManagement):
         self.__try_initialize_tapo_c500()
 
     def __try_initialize_tapo_c200(self):
-        if self.__tapo_c200 is not None:
-            return
-
         try:
             self.__tapo_c200 = Tapo(env.TAPO_C200_IP, env.TAPO_USERNAME, env.TAPO_PASSWORD)
         except Exception as e:
@@ -31,9 +28,6 @@ class TapoManagementImpl(TapoManagement):
             self.__tapo_c200 = None
 
     def __try_initialize_tapo_c500(self):
-        if self.__tapo_c500 is not None:
-            return
-
         try:
             self.__tapo_c500 = Tapo(env.TAPO_C500_IP, env.TAPO_USERNAME, env.TAPO_PASSWORD)
         except Exception as e:
